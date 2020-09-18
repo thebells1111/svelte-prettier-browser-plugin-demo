ed0f1942bc8b3bdfd27947d7614d6077b42ac100
Steven Bell <thebells1111@gmail.com>
2020-09-17 21:06:57 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# I want Prettier... for Svelte... in the Browser!!!
+
+That's what I'm working on with this. You can help.
+
+Maybe some testing. I don't know anything about testing.
+
+I'm muddling through making this work with `prettier/standalone.js`.
+
+I'm not quite sure how all of this `AST` stuff works. Maybe you know, and can help make this thing super awesome.
---
+++ src/App.svelte
@@ -0,0 +1,46 @@
+<script>
+  import prettier from "prettier";
+  import sveltePlugin from "./plugin.js";
+
+  let code;
+  code = `<div><p>Hello</p>
+  <button>World</button></div>`;
+  code =
+    `<script>	let jobNames = ['Job1', 'Job2', 'Job3']
+	let jobHours = Array.from(Array(3), () => Array.from(Array(2)));	
+	let cell = Array.from(Array(3), () => Array.from(Array(3)));
+	let newText = false
+	
+	function changeInput(e) {
+    let value = e.target.value;
+    let input = value.slice(-1); 
+		if (newText) {
+      newText = false;
+      e.target.value = input;
+    }
+
+    if (e.target.type === 'number') {
+      if (!Number(input) && Number(input) !== 0) {
+        e.target.value = '';
+      }
+
+      if (Number(value) > 24) {
+        e.target.value = input;
+      }
+    }
+		
+		console.log(jobNames)
+  }<` +
+    "/" +
+    "script>";
+
+  function format() {
+    code = prettier.format(code, {
+      parser: "svelte",
+      plugins: [sveltePlugin],
+    });
+  }
+</script>
+
+<textarea bind:value={code} style="height:50%; width: 50%; margins: 1em auto" />
+<button on:click={format}>FORMAT</button>
